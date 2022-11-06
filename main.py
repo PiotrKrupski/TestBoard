9b90d3ff41687ef7c9b26dc4f2b1994b2ba352a1
Piotr Krupski <krupskipiotr@hotmail.com>
2022-11-06 14:19:03 +0100

    expceptions added

--- main.py
+++ main.py
@@ -12,14 +12,16 @@ class RemoteConnect:
         self.__connect()
     
     def __connect(self):
-
-        self.client = SSHClient()
-        #self.client.load_system_host_keys()
-        self.client.set_missing_host_key_policy(AutoAddPolicy())
-        self.client.connect(hostname=self.host, 
-                            username=self.username, 
-                            password=self.password)
-        self.scp = SCPClient(self.client.get_transport())
+        try:
+            self.client = SSHClient()
+            #self.client.load_system_host_keys()
+            self.client.set_missing_host_key_policy(AutoAddPolicy())
+            self.client.connect(hostname=self.host,
+                                username=self.username,
+                                password=self.password)
+            self.scp = SCPClient(self.client.get_transport())
+        except Exception:
+            print("Authentication Failed: Please check your network/ssh key")
         return self.client
         
     def disconnect(self):
@@ -36,12 +38,14 @@ class RemoteConnect:
             return None
                 
     def transfer(self,file,remotepath):
-        if self.client is None:
-            self.client = self.__connect()
-        self.scp.put(file,
-                        remotepath,
-                        recursive=True)
-
+        try:
+            if self.client is None:
+                self.client = self.__connect()
+            self.scp.put(file,
+                            remotepath,
+                            recursive=True)
+        except Exception:
+            print("SCPException: Failed transferring data")
 
 # ssh = RemoteConnect(host="192.168.0.15")
 print("test")
